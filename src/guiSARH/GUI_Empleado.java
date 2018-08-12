0d3a1a8b6cc2a058819076e186bca1c90677b624
marcechelo25@hotmail.com <p@R@noid142010>
2018-08-12 17:21:07 -0500

    cedula

--- src/guiSARH/GUI_Empleado.java
+++ src/guiSARH/GUI_Empleado.java
@@ -2,6 +2,8 @@ package guiSARH;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -17,7 +19,7 @@ import javax.swing.border.LineBorder;
 import java.awt.Color;
 import javax.swing.JButton;
 
-public class GUI_Empleado extends JFrame {
+public class GUI_Empleado extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
 	private JTextField txt_cedula;
@@ -35,11 +37,17 @@ public class GUI_Empleado extends JFrame {
 	private JTextField textField_3;
 	private JTextField textField_4;
 	private JTextField textField_5;
+	
+	// variables
+	
+	String cedula = "";
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -49,9 +57,60 @@ public class GUI_Empleado extends JFrame {
 					e.printStackTrace();
 				}
 			}
+		
 		});
+		
+			
 	}
+	
+	public boolean validadorDeCedula(String cedula) {
+		boolean cedulaCorrecta = false;
+		 
+		try {
+		 
+		if (cedula.length() == 10) // ConstantesApp.LongitudCedula
+		{
+		int tercerDigito = Integer.parseInt(cedula.substring(2, 3));
+		if (tercerDigito < 6) {
+		// Coeficientes de validación cédula
+		// El decimo digito se lo considera dígito verificador
+		 int[] coefValCedula = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+		 int verificador = Integer.parseInt(cedula.substring(9,10));
+		 int suma = 0;
+		 int digito = 0;
+		for (int i = 0; i < (cedula.length() - 1); i++) {
+		 digito = Integer.parseInt(cedula.substring(i, i + 1))* coefValCedula[i];
+		 suma += ((digito % 10) + (digito / 10));
+		}
+		 
+		if ((suma % 10 == 0) && (suma % 10 == verificador)) {
+		 cedulaCorrecta = true;
+		}
+		else if ((10 - (suma % 10)) == verificador) {
+		 cedulaCorrecta = true;
+		} else {
+		 cedulaCorrecta = false;
+		}
+		} else {
+		cedulaCorrecta = false;
+		}
+		} else {
+		cedulaCorrecta = false;
+		}
+		} catch (NumberFormatException nfe) {
+		cedulaCorrecta = false;
+		} catch (Exception err) {
+		System.out.println("Una excepcion ocurrio en el proceso de validadcion");
+		cedulaCorrecta = false;
+		}
+		 
+		if (!cedulaCorrecta) {
+		System.out.println("La Cédula ingresada es Incorrecta");
+		}
+		return cedulaCorrecta;
+		}
 
+	
 	/**
 	 * Create the frame.
 	 */
@@ -257,5 +316,19 @@ public class GUI_Empleado extends JFrame {
 		JLabel label_6 = new JLabel("/");
 		label_6.setBounds(220, 255, 15, 14);
 		contentPane.add(label_6);
+		
+		JLabel label_cedula = new JLabel("");
+		label_cedula.setBounds(239, 42, 46, 14);
+		contentPane.add(label_cedula);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if (this.validadorDeCedula(this.txt_cedula.getText())) {
+		
+		}
+		
+		
 	}
 }
